a2eec0bc676c0e3d7adaa026581d8293e71a3d5c
Rerismildon <112702730+Rerismildon@users.noreply.github.com>
2023-09-04 11:00:16 -0300

    Version 0.1
    Login esta funcionando.

---
+++ AulaWeb/views/autenticacao.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render,redirect
+from django.contrib.auth import login
+
+
+def logar(request):
+    frm = AuthenticationForm(request, data=request.POST)
+
+    if frm.is_valid():
+        login(request, frm.get_user())
+        return redirect(request.GET.get('next','home'))
+
+    return render(request, "autenticacao/logar.html", {
+        'frm': frm
+    })
---
+++ bd.sql
@@ -0,0 +1,1 @@
+create database aulaWEB2108_autenticacao;
